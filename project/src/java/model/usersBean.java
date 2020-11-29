4b0b79887929dcb4fffda987c1135f09d9beeb78
Reece Perrott <reece.perrott@gmail.com>
2020-11-29 13:14:57 +0000

    created addUser() method

--- project/src/java/model/usersBean.java
+++ project/src/java/model/usersBean.java
@@ -42,4 +42,42 @@ public class usersBean {
         }//try
         return sb.toString();
     }  
+    
+    /*
+        Description: Add new user to db
+        @param: String array (data) - user data to be added to table (UNAME, PASSWORD, ROLE)
+        @return: int (flag) - returns an integer (1) based on whether a user was added to the db
+    */
+    public int addUser(String[] data){
+        
+        int flag = 0;
+        try{
+            //Check if user already exists in db
+            String query = "SELECT * FROM USERS WHERE UNAME='" + data[0].trim() + "'";
+            state = con.createStatement();
+            rs = state.executeQuery(query);
+            
+            //If user doesn't exist - add to db
+            if(!rs.next()){
+                state.close();
+                rs.close();
+                
+                //Format user data
+                query = "INSERT INTO USERS VALUES(" + data[0].trim() + "," + data[1].trim() + "," + data[2].trim() + ")";
+                state = con.createStatement();
+                //Insert
+                flag = state.executeUpdate(query);
+                
+            }
+            
+            state.close();
+        } catch (SQLException e) {
+            System.err.println("Error: " + e);
+
+        }//try
+        
+        return flag;
+    }
+    
+    
 }
