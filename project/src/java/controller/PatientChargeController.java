1f6677f58d1015d32025801543c891252f978248
CarlBeeston <73106148+CarlBeeston@users.noreply.github.com>
2021-01-19 14:14:16 +0000

    Created mehtod

--- project/src/java/controller/PatientChargeController.java
+++ project/src/java/controller/PatientChargeController.java
@@ -5,10 +5,45 @@
  */
 package controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Charges;
+import model.DBCharges;
+
 /**
  *
  * @author carl
  */
 public class PatientChargeController {
     
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+     
+        DBCharges chr = new DBCharges();
+        
+        ArrayList<Charges> charges;
+        charges = chr.getCharges();
+        request.setAttribute("charges", charges);
+        
+        RequestDispatcher view = request.getRequestDispatcher("PatientCharges.jsp");
+        view.forward(request, response);
+    }
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+    }
+}
 }
