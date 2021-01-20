228c01e6c14ac2cb7dd0d98b973d5e340ecebb0c
Reece Perrott <reece.perrott@gmail.com>
2021-01-20 16:05:51 +0000

    Added getBookings methods

--- project/src/java/model/Booking.java
+++ project/src/java/model/Booking.java
@@ -5,10 +5,60 @@
  */
 package model;
 
+import java.sql.Date;
+import java.sql.Time;
+
 /**
  *
  * @author Reece
  */
 public class Booking {
+    
+    private String eID;         //Employee ID
+    private String cID;         //Client ID
+    private String bookingDate;
+    private String bookingTime;
+
+    public Booking(String eID, String cID, String bookingDate, String bookingTime) {
+        this.eID = eID;
+        this.cID = cID;
+        this.bookingDate = bookingDate;
+        this.bookingTime = bookingTime;
+    }
+
+    public String geteID() {
+        return eID;
+    }
+
+    public void seteID(String eID) {
+        this.eID = eID;
+    }
+
+    public String getcID() {
+        return cID;
+    }
+
+    public void setcID(String cID) {
+        this.cID = cID;
+    }
+
+    public String getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(String bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public String getBookingTime() {
+        return bookingTime;
+    }
+
+    public void setBookingTime(String bookingTime) {
+        this.bookingTime = bookingTime;
+    }
+
+  
+    
     
 }
--- project/src/java/model/BookingBean.java
+++ project/src/java/model/BookingBean.java
@@ -11,6 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 /**
  *
@@ -76,6 +77,135 @@ public class BookingBean {
         return flag;
     }
     
+    public ArrayList<Booking> getAllBookings()throws SQLException{
     
+        try{
+            if(connect()){
+                //Get list of users
+                String query = "SELECT * FROM BOOKING_SLOTS";
+                state = con.createStatement();
+                rs = state.executeQuery(query);
+
+                ArrayList<Booking> listBooking = new ArrayList<>();
+                
+                //For each row in table create user & add to list
+                while(rs.next()){
+                    String eID = rs.getString("EID");
+                    String cID = rs.getString("CID");
+                    String sDate = rs.getString("SDATE");
+                    String sTime = rs.getString("STIME");
+
+                    Booking booking = new Booking(eID, cID, sDate, sTime);
+                    listBooking.add(booking);
+
+                }
+
+                rs.close(); state.close();
+                disconnect();
+
+                return listBooking;            
+            }
+        } catch (SQLException e) {
+            System.err.println("Error: " + e);
+            
+        }//try
+        
+        return null;
+        
+    }
+    
+    public ArrayList<Booking> getAllBookingsByID(String id, String userType)throws SQLException{
+        int flag = 0;
+        
+        try{
+            if(connect()){
+                //Get list of users
+                if (userType == "Employee"){
+                
+                    String query = "SELECT * FROM BOOKING_SLOTS WHERE EID=" + id;
+                    state = con.prepareStatement(query);
+                    rs = state.executeQuery(query);
+        
+                  
+                }
+                else{
+                    String query = "SELECT * FROM BOOKING_SLOTS WHERE CID=" + id;
+                    state = con.prepareStatement(query);
+                    rs = state.executeQuery(query);
+                
+                }
+                
+                ArrayList<Booking> listBooking = new ArrayList<>();
+                    
+                    while(rs.next()){
+                        String eID = rs.getString("EID");
+                        String cID = rs.getString("CID");
+                        String sDate = rs.getString("SDATE");
+                        String sTime = rs.getString("STIME");
+
+                        Booking booking = new Booking(eID, cID, sDate, sTime);
+                        listBooking.add(booking);
+
+                    }
+
+                rs.close(); state.close();
+                disconnect();
+
+                return listBooking;            
+            }
+        } catch (SQLException e) {
+            System.err.println("Error: " + e);
+            
+        }//try
+        
+        return null;
+        
+    }
+    
+    public int deleteBooking(String bookingID) throws SQLException{
+        int flag = 0;
+        try{
+            if(connect()){
+                String query = "DELETE FROM BOOKING_SLOTS WHERE SID=?";
+
+                ps = con.prepareStatement(query);
+                ps.setString(1, bookingID);
+                flag = ps.executeUpdate();
+
+                ps.close();
+                disconnect();
+
+            }            
+        } catch (SQLException e) {
+            System.err.println("Error: " + e);
+            
+        }//try
+        
+        return flag;
+    }
+    
+    public int updateBooking(Booking booking, String bookingID) throws SQLException{
+        int flag = 0;
+        try{
+            if(connect()){
+                String query = "UPDATE BOOKING_SLOTS SET EID=?, CID=?, SDATE=?, STIME=? WHERE SID=?";
+
+                ps = con.prepareStatement(query);
+                ps.setString(1, booking.geteID());
+                ps.setString(2, booking.getcID());
+                ps.setString(3, booking.getBookingDate());
+                ps.setString(4, booking.getBookingTime());
+                ps.setString(5, bookingID);
+                
+                flag = ps.executeUpdate();
+                ps.close();
+                disconnect();
+            }
+        } catch (SQLException e) {
+            System.err.println("Error: " + e);    
+        }//try
+        
+        return flag;
+    }
     
 }
